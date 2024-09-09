d6d5c6ed81b2e2566136463ded85f79eef21f4a1
Konlll <hungarokornel@gmail.com>
2024-09-09 09:16:47 +0200

    2024.09.09.

--- WPF_Brickstore/MainWindow.xaml.cs
+++ WPF_Brickstore/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace WPF_Brickstore
 
         private void btnLoadFile_Click(object sender, RoutedEventArgs e)
         {
+            legoItems = new();
             OpenFileDialog ofd = new OpenFileDialog();
             cbFields.Items.Clear();
             ofd.Filter = "BSX Files |*.bsx";
@@ -35,8 +36,6 @@ namespace WPF_Brickstore
             if (ofd.FileName.Length > 0)
             {
                 XDocument xml = XDocument.Load(ofd.FileName);
-                cbFields.Items.Add("Any");
-                cbFields.SelectedIndex = 0;
                 foreach(var xmlKey in xml.Descendants("Item").First().Elements().Select(e => e.Name.LocalName))
                 {
                     cbFields.Items.Add(xmlKey);
@@ -46,24 +45,26 @@ namespace WPF_Brickstore
                     LegoItem newItem = new(item.Element("ItemID")?.Value, item.Element("ItemTypeID")?.Value, Convert.ToInt32(item.Element("ColorID")?.Value), item.Element("ItemName")?.Value, item.Element("ItemTypeName")?.Value, item.Element("ColorName")?.Value, Convert.ToInt32(item.Element("CategoryID")?.Value), item.Element("CategoryName")?.Value, item.Element("Status")?.Value, Convert.ToInt32(item.Element("Qty")?.Value), item.Element("Price")?.Value != "0.000" ? Convert.ToDouble(item.Element("Price")?.Value) : 0, item.Element("Condition")?.Value, Convert.ToInt32(item.Element("OrigQty")?.Value));
                     legoItems.Add(newItem);
                 }
+                txtLegoItems.Text = $"Lego items ({legoItems.Count.ToString()})";
                 dgLegoItems.ItemsSource = legoItems.Select(x => new LegoViewModell(x.ItemID, x.ItemName, x.CategoryName, x.ColorName, Convert.ToInt32(x.Qty))).ToList();
             }
         }
 
-        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(txtFilter.Text != "")
+            if(cbFilter.SelectedItem != null)
             {
-                if (cbFields.SelectedItem == "Any")
-                {
-                    dgLegoItems.ItemsSource = legoItems.Where(item => item.GetType().GetProperties().Any(prop => prop.GetValue(item)?.ToString().ToLower().Contains(txtFilter.Text.ToString().ToLower()) == true)).Select(x => new LegoViewModell(x.ItemID, x.ItemName, x.CategoryName, x.ColorName, Convert.ToInt32(x.Qty))).ToList();
-                }
-                else
-                {
-                    string selectedItem = cbFields.SelectedItem.ToString();
-                    dgLegoItems.ItemsSource = legoItems.Where(item => item.GetType().GetProperty(selectedItem)?.GetValue(item)?.ToString().ToLower()?.Contains(txtFilter.Text.ToString().ToLower()) == true).Select(x => new LegoViewModell(x.ItemID, x.ItemName, x.CategoryName, x.ColorName, Convert.ToInt32(x.Qty))).ToList();
-                }
+                string selectedItem = cbFilter.SelectedItem.ToString();
+                dgLegoItems.ItemsSource = legoItems.Where(item => item.GetType().GetProperty(cbFields.SelectedItem.ToString())?.GetValue(item)?.ToString().ToLower() == selectedItem.ToLower()).Select(x => new LegoViewModell(x.ItemID, x.ItemName, x.CategoryName, x.ColorName, Convert.ToInt32(x.Qty))).ToList();
             }
+        }
+
+        private void cbFields_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dgLegoItems.ItemsSource = legoItems.Select(x => new LegoViewModell(x.ItemID, x.ItemName, x.CategoryName, x.ColorName, Convert.ToInt32(x.Qty))).ToList(); ;
+            string selectedProperty = cbFields.SelectedItem.ToString();
+            List<string> listboxItems = legoItems.Select(x => x.GetType().GetProperty(selectedProperty).GetValue(x).ToString()).ToList();
+            cbFilter.ItemsSource = listboxItems.Distinct().Order();
         }
     }
 }
